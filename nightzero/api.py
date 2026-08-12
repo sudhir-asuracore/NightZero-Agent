143769881d485e2439aa9387f0a34574ceb5d62a
Sudhir <sudhirbabunakka@asuracore.com>
2026-08-12 15:39:36 +0530

    Dynamically allow CORS origins

--- nightzero/api.py
+++ nightzero/api.py
@@ -192,7 +192,8 @@ class AgentApiHandler(BaseHTTPRequestHandler):
         self.send_response(status)
         self.send_header("Content-Type", "application/json; charset=utf-8")
         self.send_header("Content-Length", str(len(body)))
-        self.send_header("Access-Control-Allow-Origin", os.environ.get("NIGHTZERO_CORS_ORIGIN", "http://localhost:5173"))
+        origin = self.headers.get("Origin") or "*"
+        self.send_header("Access-Control-Allow-Origin", os.environ.get("NIGHTZERO_CORS_ORIGIN", origin))
         self.send_header("Access-Control-Allow-Headers", "Content-Type, Authorization")
         self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
         self.end_headers()
